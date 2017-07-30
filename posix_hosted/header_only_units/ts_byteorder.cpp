b71a48ce5e9c58d937243bf7e439dfd6655d41c3
Franz Hollerer <f.hollerer@gmx.net>
2017-07-30 19:03:56 +0200

    initial Makefile for hosted tests

---
+++ posix_hosted/header_only_units/ts_byteorder.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2017, Franz Hollerer.
+// This code is licensed under the MIT License (MIT).
+// See LICENSE file for full details.
+#include <iostream>
+
+int main()
+{
+    std::cout << "ts_byteorder\n";
+}
+
+
