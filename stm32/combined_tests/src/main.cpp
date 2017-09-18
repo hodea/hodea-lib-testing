06e05d84e88b002802d8869fcb1409ff79614abf
Franz Hollerer <f.hollerer@gmx.net>
2017-09-18 22:47:24 +0200

    infrastructure for basic device configuration

--- stm32/combined_tests/src/main.cpp
+++ stm32/combined_tests/src/main.cpp
@@ -1,4 +1,6 @@
 
+#include <hodea/device/stm32/stm32_setup.hpp>
+
 volatile int cnt = 0;
 
 int main()
---
+++ stm32/combined_tests/src/stm32f334/hodea_user_config.hpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2017, Franz Hollerer. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// See LICENSE file for full details.
+
+/**
+ * User specific configuration for the HODEA source code library.
+ *
+ * \author f.hollerer@gmx.net
+ */
+#if !defined _HODEA_USER_CONFIG_HPP_
+#define _HODEA_USER_CONFIG_HPP_
+
+#endif /*!_HODEA_USER_CONFIG_HPP_ */
