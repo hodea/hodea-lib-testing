a3c3e1f8d9c060c279c220d54b6c7a748f20d7be
Franz Hollerer <f.hollerer@gmx.net>
2017-12-01 20:14:26 +0100

    use SPDX license identifiers

--- posix_host/core/main.cpp
+++ posix_host/core/main.cpp
@@ -1,6 +1,5 @@
-// Copyright (c) 2017, Franz Hollerer. All rights reserved.
-// This code is licensed under the MIT License (MIT).
-// See LICENSE file for full details.
+// Copyright (c) 2017, Franz Hollerer.
+// SPDX-License-Identifier: MIT
 
 #define CATCH_CONFIG_MAIN // tell Catch to provide a main()
 #include <catch/catch.hpp>
--- posix_host/show_coverage.sh
+++ posix_host/show_coverage.sh
@@ -1,6 +1,5 @@
-# Copyright (c) 2017, Franz Hollerer. All rights reserved.
-# This code is licensed under the MIT License (MIT).
-# See LICENSE file for full details.
+# Copyright (c) 2017, Franz Hollerer.
+# SPDX-License-Identifier: MIT
 
 # This scrip runs a program which compiled with clang for code coverage
 # analysis and creates a report form generated profiling data.
